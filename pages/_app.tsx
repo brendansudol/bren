f8b9ad4e308e8f91748e81c8ffaaf441428ce07f
brendan sudol <brendansudol@gmail.com>
2023-01-22 17:26:01 -0500

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css"
+import { JetBrains_Mono } from "@next/font/google"
+import type { AppProps } from "next/app"
+
+const mono = JetBrains_Mono({
+  style: ["normal"],
+  subsets: ["latin"],
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${mono.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  )
 }
