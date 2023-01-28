e0ee6c4de1f519e9ebf1b255c07a6e4a83edcd9b
brendan sudol <brendansudol@gmail.com>
2023-01-28 10:31:21 -0500

    configure redirects

--- next.config.js
+++ next.config.js
@@ -1,6 +1,36 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: "/card-counting-game",
+        destination: "https://brendansudol.github.io/card-counting-game/",
+        permanent: true,
+      },
+      {
+        source: "/conan-jokes",
+        destination: "https://brendansudol.github.io/conan-jokes/",
+        permanent: true,
+      },
+      {
+        source: "/neural-jokes",
+        destination: "https://punchlines.ai/",
+        permanent: true,
+      },
+      {
+        source: "/writing",
+        destination: "https://brendansudol.github.io/writing/",
+        permanent: true,
+      },
+      {
+        source: "/writing/:slug*",
+        destination: "https://brendansudol.github.io/writing/:slug*",
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
